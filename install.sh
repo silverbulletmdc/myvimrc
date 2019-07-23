fe5a7aae65e6f6fc1c2f209311bb76c5b0704ea3
mengdechao <mengdechaolive@qq.com>
2019-07-23 19:03:04 +0800

    add bashrc

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@ ln -s `pwd`/.vimrc ~/
 ln -s `pwd`/.ideavimrc ~/
 ln -s `pwd`/.common_keybind.vim ~/
 ln -s `pwd`/.tmux.conf ~/
+ln -s `pwd`/.windows_bashrc ~/.bashrc
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
